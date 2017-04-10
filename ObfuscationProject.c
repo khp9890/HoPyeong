8215c0679b78cfc3d7b633a1fc8d8550aca7795d
khp9890 <khp9890@gmail.com>
2017-04-10 17:33:50 +0900

    Update ObfuscationProject.c

--- ObfuscationProject.c
+++ ObfuscationProject.c
@@ -8,23 +8,29 @@
 #include <stdlib.h>
 
 FILE *fp;
+FILE *ofp;
 
 void InputFilePath(char arr[COL_VAL]);												//파일경로를 입력 받음
 int InputCode(char Path[COL_VAL], char arr[ROW_VAL][COL_VAL]);						//소스코드입력
 void PrintArray(char arr[ROW_VAL][COL_VAL]);										//입력받은 코드를 출력
 void CodeAnalysis(char arr[ROW_VAL][COL_VAL], char arr2[COL_VAL][COL_VAL / 2]);		//코드 내의 변수를 인식하고 변수명을 입력
 void PrintVariable(char arr[ROW_VAL][COL_VAL / 2]);									//입력받은 변수를 출력
+void StraightCode(char arr[ROW_VAL][COL_VAL], char arr2[ROW_VAL * 20]);				//난독화된 코드를 1차원 배열에 한줄로 입력
+void Output(char arr[ROW_VAL * 20]);												//최종 난독화된 코드를 O_test.html로 저장
+
 
 int main() {
 	int iter, iter2 = 0;
-	int Existence;
+	int Existence, Existence2;
 
-	char I_FilePass[COL_VAL] = { 0 };				//파일 경로를 입력 받을 1차배열
+	char I_FilePath[COL_VAL] = { 0 };				//파일 경로를 입력 받을 1차배열
+	char O_FilePath[COL_VAL] = { 0 };				//저장 할 파일 경로를 입력 받을 1차배열
 	char I_Data[ROW_VAL][COL_VAL] = { 0 };			//입력받는 파일데이터를 담을 2차배열
 	char I_Variable[COL_VAL][COL_VAL / 2] = { 0 };	//파일 데이터를 통해 얻은 소스의 변수명를 담을 2차배열
-	
-	InputFilePath(I_FilePass);
-	Existence = InputCode(I_FilePass, I_Data);
+	char O_Data[ROW_VAL * 20] = { 0 };				//난독화된 소스를 한줄로 나열하여 저장할 1차 배열
+
+	InputFilePath(I_FilePath);
+	Existence = InputCode(I_FilePath, I_Data);
 
 	if (Existence == 0) {
 		printf("====================================================\n");
@@ -41,6 +47,8 @@ int main() {
 		PrintArray(I_Data);
 		CodeAnalysis(I_Data, I_Variable);
 		PrintVariable(I_Variable);
+		StraightCode(I_Data, O_Data);
+		Output(O_Data);
 	}
 
 	return 0;
@@ -51,7 +59,7 @@ void InputFilePath(char arr[COL_VAL]) {
 	int count = 0;
 	char temp[COL_VAL] = { 0 };
 
-	printf("Enter the file pass : ");
+	printf("Enter the file path : ");
 	scanf("%s[^\n]", arr);
 
 	for (iter = 0; iter < COL_VAL; iter++) {
@@ -59,7 +67,7 @@ void InputFilePath(char arr[COL_VAL]) {
 			count = iter + 1;
 			temp[iter2] = arr[count];
 			arr[count] = arr[iter];
-			for (count+=1; count < COL_VAL; count++) {
+			for (count += 1; count < COL_VAL; count++) {
 				temp[++iter2] = arr[count];
 				arr[count] = temp[iter2 - 1];
 			}
@@ -125,7 +133,7 @@ void CodeAnalysis(char arr[ROW_VAL][COL_VAL], char arr2[COL_VAL][COL_VAL / 2]) {
 
 void PrintVariable(char arr[ROW_VAL][COL_VAL / 2]) {
 	int iter, iter2;
-	
+
 	printf("====================================================\n");
 	printf("                  Variable Print\n");
 	printf("====================================================\n");
@@ -138,4 +146,35 @@ void PrintVariable(char arr[ROW_VAL][COL_VAL / 2]) {
 			printf("%c", arr[iter][iter2]);						//입력받은 변수명을 출력
 		}printf("\n");
 	}printf("\n");
+}
+
+void StraightCode(char arr[ROW_VAL][COL_VAL], char arr2[ROW_VAL * 20]) {
+	int iter, iter2, iter3 = 0;
+
+	for (iter = 0; iter < ROW_VAL; iter++) {
+		for (iter2 = 0; iter2 < COL_VAL; iter2++) {
+			if (arr[iter][iter2] == 0x0a)						//0x0a(한줄아래)이면 다음행으로 넘어감
+				break;
+			arr2[iter3++] = arr[iter][iter2];
+		}
+	}
+}
+
+void Output(char arr[ROW_VAL * 20]) {
+	int iter;
+	char O_FilePath[COL_VAL] = { 0 };				//저장 할 파일 경로를 입력 받을 1차배열
+
+	InputFilePath(O_FilePath);
+
+	fopen_s(&ofp, O_FilePath, "a");
+
+	fputs(arr, ofp);
+
+	fclose(ofp);
+
+	printf("====================================================\n");
+	printf("                    Output Data\n");
+	printf("====================================================\n");
+
+	puts(arr);
 }
