3c3ac5c423a30721e2059ff82e8333ceb4832270
khp9890 <khp9890@gmail.com>
2017-11-08 17:33:17 +0900

    Create HtmlFile.h

---
+++ HtmlFile.h
@@ -0,0 +1,35 @@
+#include <string>
+#include <cstring>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <map>
+#include <locale>
+#include <codecvt>
+#include <iostream>
+#include <Windows.h>
+#include "JSFile.h"
+using namespace std;
+class HtmlFile {
+private:
+ JSFile* jsFile;
+ string filename;
+ string dest_filename;
+ FILE *rfile;
+ ofstream wStream;
+ 
+ char* readline();
+     
+public:
+ class HtmlFile (string fn) {
+  filename = fn;
+  jsFile = new JSFile(filename);
+  
+ }
+ ~HtmlFile() {
+  //free all variable
+ }
+ bool openFile();
+ //bool setFile(FILE* readFile, FILE* writeFile);
+ void obfuscation();
+};
